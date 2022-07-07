e63fe07983d62f71eac14433c9ff3348bab9d4ae
arjun <arjunpuneeth@gmail.com>
2022-07-07 23:37:44 +0530

    mavan project

---
+++ src/main/java/flipzart_login/App.java
@@ -0,0 +1,13 @@
+package flipzart_login;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
